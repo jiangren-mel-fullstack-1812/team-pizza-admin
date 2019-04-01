3bca1613883f22e6941a266512238344a4f92470
Juntao Zeng <juntao.code@gmail.com>
2019-04-01 12:38:57 +1100

    create ui prototype files

---
+++ src/components/PizzaCreatorAdmin/PizzaCreatorAdmin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './PizzaCreatorAdmin.css';
+import Sidebar from '../Sidebar';
+
+const PizzaCreatorAdmin = () => (
+  <div className='pizzaCreatorAdmin'>
+    <Sidebar />
+  </div>
+);
+
+export default PizzaCreatorAdmin;
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => (
+  <div className='sidebar'>
+    <div>
+      <span className='sidebarHead'>Dashboard</span>
+    </div>
+  </div>
+);
+
+export default Sidebar;
